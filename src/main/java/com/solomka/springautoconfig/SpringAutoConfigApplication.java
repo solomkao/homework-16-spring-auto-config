e99a4da27004d561f25b793e44a591fbbb5ca423
Oksana <solomkao@gmail.com>
2021-01-22 22:27:13 +0200

    Add class ConfigurationEnabler and spring.factories, put class  ConfigurationEnabler to the classpath

--- src/main/java/com/solomka/springautoconfig/SpringAutoConfigApplication.java
+++ src/main/java/com/solomka/springautoconfig/SpringAutoConfigApplication.java
@@ -1,7 +1,18 @@
 package com.solomka.springautoconfig;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+/*
+Generate Spring Boot application using Spring Initialzr.
+Create your own auto-configuration. It should work by the following rules:
+Auto-configuration will be enabled if class ConfigurationEnabler is in the classpath;
+Auto-configuration has bean than will be created if
+‘application.properties’ file contains property “custom.configuration.enabled”
+with the value true.
+Provide an executable jar for your application.
+ */
 
 @SpringBootApplication
 public class SpringAutoConfigApplication {
