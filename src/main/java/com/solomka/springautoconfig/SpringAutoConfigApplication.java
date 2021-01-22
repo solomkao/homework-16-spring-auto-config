713030b675a76cc44f5af553f825b7019062c9bd
Oksana <solomkao@gmail.com>
2021-01-22 22:12:05 +0200

    Create new branch Add gitignore, application.properties, main class

---
+++ src/main/java/com/solomka/springautoconfig/SpringAutoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.solomka.springautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutoConfigApplication.class, args);
+    }
+
+}
